5931ac8d36316e3af55796d07179cef97357305a
Vinicius Almeid4 <cmpalmeidaa@gmail.com>
2026-03-17 21:49:46 -0300

    getters + setters e Repository

--- src/main/java/com/ex1/exercicio1/models/Livro.java
+++ src/main/java/com/ex1/exercicio1/models/Livro.java
@@ -17,4 +17,39 @@ public class Livro {
     private String autor;
 
     private Integer anoPublicacao;
+
+    public Livro() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Integer getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(Integer anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
 }
---
+++ src/main/java/com/ex1/exercicio1/repositories/LivroRepository.java
@@ -0,0 +1,9 @@
+package com.ex1.exercicio1.repositories;
+
+import com.ex1.exercicio1.models.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Long> {
+}
